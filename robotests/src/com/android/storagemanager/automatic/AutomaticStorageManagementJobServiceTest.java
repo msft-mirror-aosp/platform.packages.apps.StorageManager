LineageOS/android_packages_apps_StorageManager

Fix test to use better flag.
Previously, the show_opt_in and enabled flags were conflated to
basically mean the same thing. Because we are now distinctly making
show_opt_in refer to showing a toggle to opt into the feature in SUW and
enabled to mean the default enabled status, this unit test needed to be
updated.

Additionally, the unit test has been renamed for clarity.

Bug: 122461924
Test: m RunStorageManagerRobotests
Change-Id: Ifc61efcc65af5becdded161048f5606c09d83013

robotests/src/com/android/storagemanager/automatic/AutomaticStorageManagementJobServiceTest.java
@@ -173,8 +173,8 @@ public class AutomaticStorageManagementJobServiceTest {
     }
 
     @Test
-    public void testASMJobRunsWithValidConditionsIfOptInNotShownButUnactivated() {
-        SystemProperties.set("ro.storage_manager.show_opt_in", "false");
+    public void testASMJobRunsWithValidConditionsIfEnabledByDefaultAndUnset() {
+        SystemProperties.set("ro.storage_manager.enabled", "true");
         assertThat(mJobService.onStartJob(mJobParameters)).isFalse();
         assertStorageManagerJobRan();
     }

msft-mirror-aosp/platform.packages.apps.StorageManager

Merged-In: Ifc61efcc65af5becdded161048f5606c09d83013
(cherry picked from commit 461e0ab8fb6e60601cd45f71e47fb258faaa3465)

robotests/src/com/android/storagemanager/automatic/AutomaticStorageManagementJobServiceTest.java
@@ -191,8 +191,8 @@ public class AutomaticStorageManagementJobServiceTest {
     }
 
     @Test
-    public void testASMJobRunsWithValidConditionsIfOptInNotShownButUnactivated() {
-        StorageManagerShadowSystemProperties.put("ro.storage_manager.show_opt_in", "false");
+    public void testASMJobRunsWithValidConditionsIfEnabledByDefaultAndUnset() {
+        StorageManagerShadowSystemProperties.put("ro.storage_manager.enabled", "true");
         assertThat(mJobService.onStartJob(mJobParameters)).isFalse();
         assertStorageManagerJobRan();
     }